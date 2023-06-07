c92a6d3b848a3ef0670f92ac99ea5461bb533d65
Jimmy <j.austin.ozu@gmail.com>
2023-06-08 08:28:58 +1200

    Publish version 0.0.2 of Odinproject Etch-a-Sketch
    Commit code for the basic version of the Etch-a-Sketch project. As of
    now, players can manually input the tile size in to the code, and if it
    is an square number, the appropriate number of tiles will show on the
    computer. Currently, when the user hovers over the tiles, the CSS backgr
    ound property changes to blue. Also added a clear button to reset the
    game. Looking into a drag and hover effect for the tiles, as well as
    a GUI input into the number of tiles as well as a color picker option.

--- app.js
+++ app.js
@@ -2,32 +2,53 @@ console.log('hello world');
 
 const container = document.getElementById('container');
 
-let divNumber = 16;
-console.log(Number.isInteger(Math.sqrt(divNumber)));
-const tileType = (divNumber) => {
-    if (Number.isInteger(Math.sqrt(divNumber))) {
-        return divNumber / Math.sqrt(divNumber)
-    } else {
-        console.log('the number is not an integer')
-    }
+const divNumber = 25;
+const tileNumber = (divNumber) => {
+    if (Number.isInteger(Math.sqrt(divNumber)))  {
+        return (divNumber / Math.sqrt(divNumber))
+    } return 'ERROR';
 }
+console.log(Number.isInteger(Math.sqrt(divNumber)));
+console.log(tileNumber(divNumber))
 
-console.log(tileType(divNumber))
 
 
 
-for (let i = 0; i < tileType(divNumber); i++) {
-    const row = document.createElement('div');
-    row.classList.add('row')
-    container.appendChild(row);
+container.style.cssText = `display: grid; grid-template-rows: repeat(${tileNumber(divNumber)}, 100px); grid-template-columns: repeat(${tileNumber(divNumber)}, 100px); justify-content: center;`
+
+const clearButton = document.createElement('button');
+clearButton.innerText = 'Clear Tiles'
+clearButton.style.cssText = 'Margin: auto; '
+
+for (let i = 0; i < (divNumber); i++) {
+    if (Number.isInteger(Math.sqrt(divNumber))) {
+        const tile = document.createElement('div');
+        tile.classList.add('tile')
+        tile.style.cssText = 'justify-self: 100%; align-self: 100%;'
+        tile.style.cssText = 'border: 1px solid black'
+        // tile.innerText = 'Tile #' + Number(i + 1);
+        container.appendChild(tile);
+        tile.addEventListener("mouseover", () => tile.style.cssText = 'background-color: blue;')
+        clearButton.addEventListener('click', () => tile.style.cssText = 'background-color: white; border: 1px solid black;')
+
+       
+    } else {
+        container.innerText = "You didn't input a square number!"
+    }
    
 }
 
-for (let j = 0; j < tileType(divNumber); i++) {
-    const tile = document.createElement('div');
-    tile.classList.add('tile')
-    row.appendChild(tile)
-}
+const tileInfo = document.querySelector('#tileinfo');
+tileInfo.innerText = `Current tile size: ${tileNumber(divNumber)} * ${tileNumber(divNumber)}`
+
+
+const main = document.getElementById('main')
+
+
+
+main.appendChild(clearButton);
+
+
 
   
 
