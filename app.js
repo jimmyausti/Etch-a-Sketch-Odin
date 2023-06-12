a924944886a4d67d16f55699a11eaf6468794307
Jimmy <j.austin.ozu@gmail.com>
2023-06-12 18:07:53 +1200

    Publish version 0.0.3 (alpha) of Etch-a-Sketch
    Commit code for the alpha version of the Etch-a-Sketch application for
    the Odin Project. Added a slider functionality that allows users to
    adjust the amount of tiles in the etch-a-sketch app. Right now the app
    just needs a few touch ups. Drag and draw functionality, initial value
    of the grid are two things that come to mind, as well as some extra
    styling of the body. But importantly I think I need to create some
    dynamic sizing of the tiles so I can get bigger sizes into the app.

--- app.js
+++ app.js
@@ -1,57 +1,88 @@
-console.log('hello world');
+console.log("hello world");
 
-const container = document.getElementById('container');
+/// slider functionality
 
-const divNumber = 25;
-const tileNumber = (divNumber) => {
-    if (Number.isInteger(Math.sqrt(divNumber)))  {
-        return (divNumber / Math.sqrt(divNumber))
-    } return 'ERROR';
-}
-console.log(Number.isInteger(Math.sqrt(divNumber)));
-console.log(tileNumber(divNumber))
+const sliderContainer = document.querySelector("#input-slider");
 
+const tileInfo = document.querySelector("#tileinfo");
+const slider = document.querySelector(".slider");
+
+const maxVal = slider.getAttribute("max");
+const val = (slider.value / maxVal) * 10;
 
 
 
-container.style.cssText = `display: grid; grid-template-rows: repeat(${tileNumber(divNumber)}, 100px); grid-template-columns: repeat(${tileNumber(divNumber)}, 100px); justify-content: center;`
+slider.addEventListener("input", () => {
+  handleSlider();
+  console.log(val)
+});
 
-const clearButton = document.createElement('button');
-clearButton.innerText = 'Clear Tiles'
-clearButton.style.cssText = 'Margin: auto; '
+////
 
-for (let i = 0; i < (divNumber); i++) {
-    if (Number.isInteger(Math.sqrt(divNumber))) {
-        const tile = document.createElement('div');
-        tile.classList.add('tile')
-        tile.style.cssText = 'justify-self: 100%; align-self: 100%;'
-        tile.style.cssText = 'border: 1px solid black'
-        // tile.innerText = 'Tile #' + Number(i + 1);
-        container.appendChild(tile);
-        tile.addEventListener("mouseover", () => tile.style.cssText = 'background-color: blue;')
-        clearButton.addEventListener('click', () => tile.style.cssText = 'background-color: white; border: 1px solid black;')
+const container = document.getElementById("container");
+const tiles = [];
 
-       
-    } else {
-        container.innerText = "You didn't input a square number!"
-    }
+function handleSlider() {
+    
+    const maxVal = slider.getAttribute("max");
+    const val = (slider.value / maxVal) * 10;
+    tileInfo.innerText = `Current tile size: ${val}`;
+    
+    const divNumber = Math.pow(val, 2);
+    const tileNumber = (divNumber) => {
+        if (Number.isInteger(Math.sqrt(divNumber))) {
+            return divNumber / Math.sqrt(divNumber);
+        }
+        return "ERROR";
+    };
+    console.log(Number.isInteger(Math.sqrt(divNumber)));
+    console.log(tileNumber(divNumber));
+    
    
-}
+    container.style.cssText = `display: grid; grid-template-rows: repeat(${tileNumber(
+        divNumber
+        )}, 100px); grid-template-columns: repeat(${tileNumber(
+            divNumber
+            )}, 100px); justify-content: center;`;
+            
+    tiles.forEach((tile) => tile.remove())
+    tiles.length = 0;
 
-const tileInfo = document.querySelector('#tileinfo');
-tileInfo.innerText = `Current tile size: ${tileNumber(divNumber)} * ${tileNumber(divNumber)}`
+for (let i = 0; i < divNumber; i++) {
+    if (Number.isInteger(Math.sqrt(divNumber))) {
+            const tile = document.createElement("div");
+            tile.classList.add("tile");
+            tile.style.cssText = "justify-self: 100%; align-self: 100%;";
+            tile.style.cssText = "border: 1px solid black";
+            // tile.innerText = 'Tile #' + Number(i + 1);
+            container.appendChild(tile);
+            tiles.push(tile);
+            tile.addEventListener(
+                "mouseover",
+                () => (tile.style.cssText = "background-color: blue;")
+                );
+                
+                    } else {
+                        container.innerText = "You didn't input a square number!";
+                    }
+    }
+            }
+            
+        
+            
+        
 
 
-const main = document.getElementById('main')
-
-
-
+        const clearButton = document.createElement("button");
+        clearButton.innerText = "Clear Tiles";
+clearButton.style.cssText = "Margin: auto; ";
+const main = document.getElementById("main");
+clearButton.addEventListener(
+    "click",
+    () =>
+    (tile.style.cssText =
+        "background-color: white; border: 1px solid black;")
+        );
+            
 main.appendChild(clearButton);
-
-
-
-  
-
-
-
 
