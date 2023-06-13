a28f7d5e00554abb38e166382d727f9107a29e89
Jimmy <j.austin.ozu@gmail.com>
2023-06-13 12:06:54 +1200

    Publish beta version of Etch-a-Sketch for OdinProj
    Commit the final version of the Etch-a-Sketch project for the Odin
    project curriculum. A color picker and dynamic height styling were added
    on. A future version of this code needs to be cleaned up.

--- app.js
+++ app.js
@@ -1,88 +1,96 @@
-console.log("hello world");
 
-/// slider functionality
+//* slider functionality
 
 const sliderContainer = document.querySelector("#input-slider");
 
 const tileInfo = document.querySelector("#tileinfo");
 const slider = document.querySelector(".slider");
 
-const maxVal = slider.getAttribute("max");
-const val = (slider.value / maxVal) * 10;
-
-
 
 slider.addEventListener("input", () => {
   handleSlider();
-  console.log(val)
 });
 
-////
+//* color picker
+
+// Define a global variable to store the selected color
+let selectedColor = '';
+
+// Get the color input and selected color elements
+const colorInput = document.getElementById('colorInput');
+
+// Add an event listener to the color input
+colorInput.addEventListener('input', () => {
+  const color = colorInput.value;
+
+  // Update the selected color variable
+  selectedColor = color;
+});
+
+// Example usage of the selectedColor variable
+
+
+
+//* main function
 
 const container = document.getElementById("container");
 const tiles = [];
 
 function handleSlider() {
-    
-    const maxVal = slider.getAttribute("max");
-    const val = (slider.value / maxVal) * 10;
-    tileInfo.innerText = `Current tile size: ${val}`;
-    
-    const divNumber = Math.pow(val, 2);
-    const tileNumber = (divNumber) => {
-        if (Number.isInteger(Math.sqrt(divNumber))) {
-            return divNumber / Math.sqrt(divNumber);
-        }
-        return "ERROR";
-    };
-    console.log(Number.isInteger(Math.sqrt(divNumber)));
-    console.log(tileNumber(divNumber));
-    
-   
-    container.style.cssText = `display: grid; grid-template-rows: repeat(${tileNumber(
-        divNumber
-        )}, 100px); grid-template-columns: repeat(${tileNumber(
-            divNumber
-            )}, 100px); justify-content: center;`;
-            
-    tiles.forEach((tile) => tile.remove())
-    tiles.length = 0;
+  const val = (slider.value); 
+  tileInfo.innerText = `Current tile size: ${val}`;
 
-for (let i = 0; i < divNumber; i++) {
+  const divNumber = Math.pow(val, 2);
+  const tileNumber = (divNumber) => {
     if (Number.isInteger(Math.sqrt(divNumber))) {
-            const tile = document.createElement("div");
-            tile.classList.add("tile");
-            tile.style.cssText = "justify-self: 100%; align-self: 100%;";
-            tile.style.cssText = "border: 1px solid black";
-            // tile.innerText = 'Tile #' + Number(i + 1);
-            container.appendChild(tile);
-            tiles.push(tile);
-            tile.addEventListener(
-                "mouseover",
-                () => (tile.style.cssText = "background-color: blue;")
-                );
-                
-                    } else {
-                        container.innerText = "You didn't input a square number!";
-                    }
+      return divNumber / Math.sqrt(divNumber);
     }
-            }
-            
-        
-            
-        
+    return "ERROR";
+  };
+  
+  container.style.cssText = `display: grid; grid-template-rows: repeat(${tileNumber(
+    divNumber
+  )}, ${720 / tileNumber(divNumber)}px); grid-template-columns: repeat(${tileNumber(
+    divNumber
+  )}, ${720 / tileNumber(divNumber)}px); justify-content: center; align-content: center;`;
+
+  tiles.forEach((tile) => tile.remove());
+  tiles.length = 0;
+
+  for (let i = 0; i < divNumber; i++) {
+    if (Number.isInteger(Math.sqrt(divNumber))) {
+      const tile = document.createElement("div");
+      tile.classList.add("tile");
+      tile.style.cssText = "justify-self: 100%; align-self: 100%;";
+      tile.style.cssText = "border: 1px solid black";
+      container.appendChild(tile);
+      tiles.push(tile);
+      tile.addEventListener(
+        "mouseover",
+        () => (tile.style.cssText = `background-color: ${selectedColor};`)
+      );
+    } else {
+      container.innerText = "You didn't input a square number!";
+    }
+  }
+  
+}
 
 
-        const clearButton = document.createElement("button");
-        clearButton.innerText = "Clear Tiles";
+//* clear button + functionality
+
+const clearButton = document.createElement("button");
+clearButton.innerText = "Clear Tiles";
 clearButton.style.cssText = "Margin: auto; ";
 const main = document.getElementById("main");
+main.appendChild(clearButton);
+
 clearButton.addEventListener(
     "click",
-    () =>
-    (tile.style.cssText =
-        "background-color: white; border: 1px solid black;")
-        );
-            
-main.appendChild(clearButton);
+    () => {
+        for (let i = 0; i < (Math.pow((slider.value), 2)); ++i) {
+            (tiles[i].style.cssText = "background-color: white; border: 1px solid black;")
+        }
+    }
+  );
 
